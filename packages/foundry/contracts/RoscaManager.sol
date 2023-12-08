77c8b5e1a501e4183508a2dd87f6cfe74d84692e
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2023-12-08 22:46:41 +0530

    Initialized manager contract

---
+++ packages/foundry/contracts/RoscaManager.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract RoscaManager is Ownable {
+    uint256 _nextGroupId;
+    mapping(uint256 => address) _groups;
+
+    address public immutable baseImplementation;
+    address public STABLECOIN_ADDRESS;
+
+    event GroupCreated(address groupAddress, address creator);
+
+    constructor(
+        address initialOwner,
+        address stablecoinAddress,
+        address baseImplementation_
+    ) Ownable(initialOwner) {
+        STABLECOIN_ADDRESS = stablecoinAddress;
+        baseImplementation = baseImplementation_;
+    }
+
+    function createGroup(uint256 amount, uint256 members) public {
+        uint256 groupId = _nextGroupId++;
+        address clone = Clones.clone(baseImplementation);
+        _groups[groupId] = clone;
+    }
+
+    function numberOfGroups() public view returns (uint256) {
+        return _nextGroupId;
+    }
+
+    function getGroup(uint256 groupId) public view returns (address) {
+        return _groups[groupId];
+    }
+}
