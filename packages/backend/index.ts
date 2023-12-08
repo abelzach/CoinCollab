1f4df7e6c9f33cb25b21891134912633f8a160a9
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2023-12-09 01:59:54 +0530

    Initialized supabase

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = 3001;
+const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_KEY as string);
 
 app.use(cors());
 app.use(express.json());
