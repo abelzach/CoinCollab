22f9be8b55e8a4ce6329aa98b76319db133edb83
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2023-12-09 23:51:41 +0530

    Updated backend with listeners

---
+++ packages/backend/contractAddress.ts
@@ -0,0 +1,14 @@
+export const contractAddress = {
+    31337: {
+        RoscaManager: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+    },
+    1442: {
+        RoscaManager: ""
+    },
+    84531: {
+        RoscaManager: ""
+    },
+    421613: {
+        RoscaManager: ""
+    }
+};
--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
-import ethers, { Wallet } from 'ethers';
+import { ethers, Contract, Wallet, Provider } from 'ethers';
 import dotenv from 'dotenv';
+import { contractAddress } from './contractAddress';
+
+const RoscaManager = require("../nextjs/abis/RoscaManager.json");
+const RoscaGroup = require("../nextjs/abis/RoscaGroup.json");
 
 dotenv.config();
 
@@ -10,42 +14,65 @@ const app = express();
 const port = 3001;
 const supabase = createClient(process.env.SUPABASE_URL as string, process.env.SUPABASE_KEY as string);
 
+const zkEvmTestnetProvider = new ethers.JsonRpcProvider(process.env.ZKEVM_TESTNET_RPC_URL);
 const baseGoerliProvider = new ethers.JsonRpcProvider(process.env.BASE_GOERLI_RPC_URL);
+const arbitrumGoerliProvider = new ethers.JsonRpcProvider(process.env.ARBITRUM_GOERLI_RPC_URL);
+const localhostProvider = new ethers.JsonRpcProvider('http://127.0.0.1:8545');
+
 
 app.use(cors());
 app.use(express.json());
 
-
-app.post('/api/create-group', async (req, res) => {
+app.get('/api/add-listener', async (req, res) => {
     try {
-        const { chain, group, address, amount, members } = req.body;
+        const { chain } = req.query as any;
         if (!chain) throw new Error('Chain not specified');
-        if (!group) throw new Error('Group ID not specified');
-        if (!address) throw new Error('Address not specified');
-        if (!amount) throw new Error('Amount not specified');
-        if (!members) throw new Error('Members not specified');
-
-        let ownerSigner: Wallet;
-        switch (chain) {
-            case 84531: ownerSigner = new ethers.Wallet(process.env.PRIVATE_KEY as string, baseGoerliProvider); break;
+        
+        let provider: Provider;
+        switch (parseInt(chain)) {
+            case 31337: provider = localhostProvider; break;
+            case 1442: provider = zkEvmTestnetProvider; break;
+            case 84531: provider = baseGoerliProvider; break;
+            case 421613: provider = arbitrumGoerliProvider; break;
             default: throw new Error('Chain not supported');
         }
 
-        const created_at = new Date().toISOString();
-        const { data, error } = await supabase
-        .from('groups')
-        .insert([{ chain, group, address, amount, members, created_at }]);
-        if (error) throw error;
-        return res.status(200).json(data);
+        const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
+        const roscaManager = new ethers.Contract(contractAddress[parseInt(chain)].RoscaManager, RoscaManager.abi, provider);
+        roscaManager.addListener('GroupCreated', async (groupAddress: string, creator: string) => {
+            console.log("New group", parseInt(chain), groupAddress);
+            const roscaGroup = new ethers.Contract(groupAddress, RoscaGroup.abi, provider);
+            const groupDetails = await (roscaGroup.connect(signer) as any).getGroupDetails();
+            console.log(groupDetails);
+            const groupMembers = await (roscaGroup.connect(signer) as any).getMembers();
+
+            roscaGroup.addListener('GroupStarted', async (timestamp: any) => {
+                console.log("Group started", parseInt(chain), groupAddress);
+                const created_at = new Date().toISOString();
+                const { data, error } = await supabase
+                .from('groups')
+                .insert([{ 
+                    chain: parseInt(chain), 
+                    group: groupDetails.id, 
+                    address: groupAddress, 
+                    amount: parseInt(ethers.formatEther(groupDetails.amount)), 
+                    members: groupMembers, 
+                    created_at 
+                }]);
+            });
+            roscaGroup.addListener('RoundStarted', async (round: any, timestamp: any) => {
+                console.log("Round started", parseInt(chain), round, groupAddress);
+            });
+        });
     } catch (error) {
         console.log(error);
         return res.status(500);
     }
-});
+});    
 
 app.get('/api/get-groups', async (req, res) => {
     try {
-      const { chain } = req.params as any;
+      const { chain } = req.query as any;
       if (!chain) throw new Error('Chain not specified');
       const { data, error } = await supabase
         .from('groups')
@@ -61,7 +88,7 @@ app.get('/api/get-groups', async (req, res) => {
 
 app.get('/api/get-group', async (req, res) => {
     try {
-      const { chain, group } = req.params as any;
+      const { chain, group } = req.query as any;
       if (!chain) throw new Error('Chain not specified');
       if (!group) throw new Error('Group ID not specified');
   
