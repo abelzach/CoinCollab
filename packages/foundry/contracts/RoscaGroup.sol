2417f9e92b6fa1eb9c5612bcb2bf6d0d53f39e5a
UltimateRoman <59477486+UltimateRoman@users.noreply.github.com>
2023-12-09 08:35:03 +0530

    Updated group contract

--- packages/foundry/contracts/RoscaGroup.sol
+++ packages/foundry/contracts/RoscaGroup.sol
@@ -8,10 +8,27 @@ import "./interfaces/IRoscaGroup.sol";
 contract RoscaGroup is IRoscaGroup, Initializable {
     address[] _members;
     GroupDetails _groupDetails;
+    mapping(uint256 => mapping(address => bool)) _memberContributed;
 
+    uint8 public constant PLATFORM_FEE = 5;
     address public STABLECOIN_ADDRESS;
     address public MANAGER_ADDRESS;
     address public owner;
+
+    mapping(address => bool) public isMember;
+
+    GroupStage public groupStage = GroupStage.INITIALIZED;
+    mapping(uint256 => RoundStage) public roundStage;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner");
+        _;
+    }
+
+    modifier onlyMember() {
+        require(isMember[msg.sender], "Only member");
+        _;
+    }
 
     constructor() {
         _disableInitializers();
@@ -39,7 +56,32 @@ contract RoscaGroup is IRoscaGroup, Initializable {
         owner = owner_;
     }
 
+    function joinGroup() external {
+        require(!isMember[msg.sender], "Already joined");
+        require(_members.length < _groupDetails.members, "Group is full");
+        require(groupStage == GroupStage.INITIALIZED, "Group started");
+
+        address newMember = msg.sender;
+        _members.push(newMember);
+        isMember[newMember] = true;
+        _groupDetails.currentMembers++;
+        if (_members.length == _groupDetails.members) {
+            groupStage = GroupStage.ONGOING;
+            _groupDetails.startTime = block.timestamp;
+            _groupDetails.currentRound = 1;
+            roundStage[_groupDetails.currentRound] = RoundStage.COLLECTION;
+        }
+    }
+
     function getGroupDetails() public view returns (GroupDetails memory) {
         return _groupDetails;
+    }
+
+    function getMembers() public view returns (address[] memory) {
+        return _members;
+    }
+
+    function getCurrentRound() public view returns (uint256) {
+        return _groupDetails.currentRound;
     }
 }
